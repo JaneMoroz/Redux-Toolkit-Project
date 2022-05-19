03d444737820ca8151ecb45f8253a2c655a4ac75
JaneMoroz <moroz.jane@yahoo.com>
2022-05-19 17:38:59 +0300

    Added Landing/Error Pages, Routing

---
+++ src/pages/Landing.js
@@ -0,0 +1,34 @@
+import main from "../assets/images/main.svg";
+import Wrapper from "../assets/wrappers/LandingPage";
+import { Logo } from "../components";
+import { Link } from "react-router-dom";
+
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        {/* info */}
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Organic tonx glossier brunch vegan humblebrag shaman forage celiac
+            four dollar toast keytar narwhal try-hard dreamcatcher. Beard
+            literally echo park hell of selfies. Microdosing forage taiyaki
+            umami occupy godard keytar locavore waistcoat praxis.
+          </p>
+          <Link to="/register" className="btn btn-hero">
+            Login/Register
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Landing;
